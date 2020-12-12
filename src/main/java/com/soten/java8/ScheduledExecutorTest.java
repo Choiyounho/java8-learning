4cfe7000b9145ae5285fedd0daa0b0e9659a49ce
Choiyounho <maxosa78@gmail.com>
2020-12-12 17:23:34 +0900

    [CompletableFuture] : Executors

--- src/main/java/com/soten/java8/ScheduledExecutorTest.java
+++ src/main/java/com/soten/java8/ScheduledExecutorTest.java
@@ -1,2 +1,24 @@
-package com.soten.java8;public class ScheduledExecutorTest {
+package com.soten.java8;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+public class ScheduledExecutorTest {
+
+    public static void main(String[] args) {
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        // 3초 후 실행
+        executorService.schedule(getRunnable("Hello"), 3, TimeUnit.SECONDS);
+
+        // 1초 후 시작 2초에 한번씩 출력
+        executorService.scheduleAtFixedRate(getRunnable("Hello"), 1, 2, TimeUnit.SECONDS);
+    }
+
+    private static Runnable getRunnable(String message) {
+        System.out.println(message + Thread.currentThread().getName());
+        return () -> System.out.println(message + Thread.currentThread().getName());
+    }
+    
 }
--- src/test/java/com/soten/java8/ExecutorsTest.java
+++ src/test/java/com/soten/java8/ExecutorsTest.java
@@ -1,2 +1,42 @@
-package com.soten.java8;public class ExecutorsTest {
+package com.soten.java8;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorsTest {
+
+    @Test
+    @DisplayName("싱글 스레드")
+    void singleTest() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor(); // 스레드를 하나만 쓰는 executor
+        executorService.submit(() -> System.out.println("Thread " + Thread.currentThread().getName()));
+
+        executorService.shutdown(); // 현재 작업 중인 작업은 다 끝마치고 종료
+        executorService.shutdownNow(); // 이 메서드를 쓰면 즉시 종료이다.
+    }
+
+    @Test
+    @DisplayName("pool 스레드")
+    void poolTest() {
+        ExecutorService executorService1 = Executors.newFixedThreadPool(2);
+        executorService1.submit(getRunnable("Hello"));
+        executorService1.submit(getRunnable("My"));
+        executorService1.submit(getRunnable("Name"));
+        executorService1.submit(getRunnable("Is"));
+        executorService1.submit(getRunnable("Younho"));
+
+        executorService1.shutdown();
+    }
+
+    private static Runnable getRunnable(String message) {
+        System.out.println(message + Thread.currentThread().getName());
+        return () -> System.out.println(message + Thread.currentThread().getName());
+    }
+
+
 }
